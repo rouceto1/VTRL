464890cc908ed194c4df579c320181d3b1a6d270
rouceto1-t14 <rouceto1@fel.cvut.cz>
2023-05-09 11:27:25 +0200

    integration of multiple steps

--- process_strategy.py
+++ process_strategy.py
@@ -34,34 +34,40 @@ evaluation_prefix = os.path.join(pwd, args.evaluation_prefix)
 
 GT_file = os.path.join(evaluation_prefix, "GT.pickle")
 notify = Notify(endpoint="https://notify.run/cRRiMSUpAEL2LLH37uWZ")
-def process(paths,REDO=False):
+
+
+def process(paths, REDO=[False, False, False, False]):
     estimates_grade = None
     for exp in paths:
-        #try:
-            print(exp)
-            experiments_path = os.path.join(pwd, "experiments",exp)
-            chosen_positions = np.loadtxt(os.path.join(experiments_path, "input.txt"), int)
-            weights_eval = os.path.join(experiments_path, "weights.pt")
-            estimates_grade_out = os.path.join(experiments_path, "estimates.pickle")
-            estimates_train_out = os.path.join(experiments_path, "train.pickle")
-            config = load_config(os.path.join(experiments_path, "NN_config.yaml"), 512)
-            if not os.path.exists(weights_eval) or REDO:
-                file_list_teach = teach(dataset_path, chosen_positions, weights_eval, conf=config)
-            if not os.path.exists(estimates_train_out) or REDO:
-                estimates_grade = evaluate_to_file(dataset_path, evaluation_prefix, evaluation_paths, weights_eval,
+        # try:
+        print(exp)
+        experiments_path = os.path.join(pwd, "experiments", exp)
+        chosen_positions = np.loadtxt(os.path.join(experiments_path, "input.txt"), int)
+        weights_eval = os.path.join(experiments_path, "weights.pt")
+        estimates_grade_out = os.path.join(experiments_path, "estimates.pickle")
+        estimates_train_out = os.path.join(experiments_path, "train.pickle")
+        config = load_config(os.path.join(experiments_path, "NN_config.yaml"), 512)
+        if not os.path.exists(weights_eval) or REDO[0]:
+            file_list_teach = teach(dataset_path, chosen_positions, weights_eval, conf=config)
+        if not os.path.exists(estimates_train_out) or REDO[1]:
+            estimates_grade = evaluate_to_file(dataset_path, evaluation_prefix, evaluation_paths, weights_eval,
                                                _estimates_out=estimates_train_out, conf=config)
-            if not os.path.exists(estimates_grade_out) or REDO:
-                with open(GT_file, 'rb') as handle:
-                    gt_in = pickle.load(handle)
-                estimates_grade = evaluate_for_GT(dataset_path, evaluation_prefix, evaluation_paths, weights_eval, _GT=gt_in,
+        if not os.path.exists(estimates_grade_out) or REDO[2]:
+            with open(GT_file, 'rb') as handle:
+                gt_in = pickle.load(handle)
+            estimates_grade = evaluate_for_GT(dataset_path, evaluation_prefix, evaluation_paths, weights_eval,
+                                              _GT=gt_in,
                                               _estimates_out=estimates_grade_out, conf=config)
-            if not os.path.exists(os.path.join(experiments_path, "input.png")) or REDO:
-                with open(GT_file, 'rb') as handle:
-                    gt_in = pickle.load(handle)
-                grade_type(experiments_path,positions=chosen_positions, _GT=gt_in,estimates_file=estimates_grade_out, estimates=estimates_grade)
-        #except:
-        #    notify.send('Something failed: ' + exp)
+        if not os.path.exists(os.path.join(experiments_path, "input.png")) or REDO[3]:
+            with open(GT_file, 'rb') as handle:
+                gt_in = pickle.load(handle)
+            grade_type(experiments_path, positions=chosen_positions, _GT=gt_in, estimates_file=estimates_grade_out,
+                       estimates=estimates_grade)
+    # except:
+    #    notify.send('Something failed: ' + exp)
     notify.send('Finished')
+
+
 if __name__ == "__main__":
-    REDO = False
-    process(["test"],REDO)
+    REDO = [True, True, True, True]
+    process(["test"], REDO)
--- process_strategy_multiple.py
+++ process_strategy_multiple.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 from process_strategy import process
 import os
+
 pwd = os.getcwd()
 experiments_path = os.path.join(pwd, "experiments")
 
 if __name__ == "__main__":
-    paths = ["one", "two","three","four","five","six","seven","eight"]
-    process(paths)
+    paths = ["all", "one", "two", "three", "four", "five", "six", "seven", "eight"]
+    teach = False
+    e_teach = False
+    e_grade = False
+    grade = True
+    redo = [teach, e_teach, e_grade, grade]
+    process(paths, redo)
