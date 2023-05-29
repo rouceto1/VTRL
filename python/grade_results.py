5deb3320ce1fc4c0b30447e90dfdff6cc0373453
rouceto1-t14 <rouceto1@fel.cvut.cz>
2023-05-29 15:57:00 +0200

    better time measuring

--- python/grade_results.py
+++ python/grade_results.py
@@ -178,10 +178,10 @@ def grade_type(dest, positions=None, estimates_file=None, _GT=None, estimates=No
     print("get gt for offset pairs")
     gt = read_gt_file(file_list, _GT)
     print("loaded GT")
-    exp_time = time.time()-time_elapsed
+    exp_time = int(time.time()-time_elapsed)
     # SOLVED: redo the compute_to_file, the gt is already sorted to the data to compare it to
     experiemnt_name = os.path.basename(os.path.normpath(dest))
-    out = [experiemnt_name,exp_time, *compute_to_file(displacements, gt, matches, dest, positions, fig_place=dest)]
+    out = [experiemnt_name, exp_time, *compute_to_file(displacements, gt, matches, dest, positions, fig_place=dest)]
     path = Path(dest).parent
 
     with open(path / 'ouput.csv', 'a') as f_object:
