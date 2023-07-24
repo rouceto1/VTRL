7090c3c86653b5c0880b6c8c5c3993161cda01cf
rouceto1-t14 <rouceto1@fel.cvut.cz>
2023-07-24 22:36:18 +0200

    eval plotting fix

--- process_strategy.py
+++ process_strategy.py
@@ -60,7 +60,7 @@ def process(paths, REDO=[False, False, False, False]):
         if not os.path.exists(estimates_grade_out) or REDO[2]:
             with open(GT_file, 'rb') as handle:
                 gt_in = pickle.load(handle)
-            estimates_grade = evaluate_for_GT(experiments_path, evaluation_prefix, evaluation_paths, weights_eval,
+            estimates_grade = evaluate_for_GT(os.path.join (experiments_path,"plots"), evaluation_prefix, evaluation_paths, weights_eval,
                                               _GT=gt_in,
                                               _estimates_out=estimates_grade_out, conf=config)
         if not os.path.exists(os.path.join(experiments_path, "plots", "input.png")) or REDO[3]:
