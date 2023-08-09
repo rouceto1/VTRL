9991c8411da20aa1df756e0432368ab258cb75cc
rouceto1-t14 <rouceto1@fel.cvut.cz>
2023-08-09 17:54:57 +0200

    data handling script

---
+++ data_backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+while true; do
+
+read -p "Where to back up data (cancel)" yn
+
+case $yn in
+	cancel ) break;;
+	* ) echo saving;
+	  mkdir backups/$yn;
+	  rsync -av --progress experiments/*  backups/$yn;
+	  break;;
+esac
+done
+
+while true; do
+read -p "Reset experiment (yes/no) " yn
+case $yn in
+    yes ) rm -rf experiments/*;
+		exit;;
+	no ) echo exiting...;
+		exit;;
+	* ) echo invalid response;;
+esac
+done
