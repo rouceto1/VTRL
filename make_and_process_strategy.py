5418071de910cb253a786272e2b061a9eab42f82
rouceto1-t14 <rouceto1@fel.cvut.cz>
2023-08-09 16:57:42 +0200

    Creation of biased training set

--- make_and_process_strategy.py
+++ make_and_process_strategy.py
@@ -20,19 +20,20 @@ class NumpyArrayEncoder(JSONEncoder):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
+
 def make_places_list_by_places(seasons, percentage_to_explore, block_size, whole_place_at_once,
-                     single_place_per_batch,
-                     dataset_weight, places, places_weights):
+                               single_place_per_batch,
+                               dataset_weight, places, places_weights):
+    percentage_to_explore = percentage_to_explore * dataset_weight
     if percentage_to_explore == 0.0:
-        #return empty array
+        # return empty array
         return make_empty_places_list(seasons, places), 0
     else:
-        percentage_to_explore = percentage_to_explore * dataset_weight
         season_count = seasons * percentage_to_explore
         # make numpy array of booleans for each place for each season
 
         rng = np.random.default_rng()
-        selected_seasons = rng.choice(seasons, size=season_count, replace=False)
+        selected_seasons = rng.choice(seasons, size=int(season_count), replace=False)
         seasons_out = []
         total_count = 0
         for s in range(seasons):
@@ -49,8 +50,11 @@ def make_places_list_by_places(seasons, percentage_to_explore, block_size, whole
                 seasons_out.append(np.zeros(places, dtype=bool))
         return seasons_out, total_count
 
+
 def bool_based_on_probability(probability=0.5):
     return random.random() < probability
+
+
 def make_empty_places_list(seasons, places):
     places_tmp = np.zeros(seasons * places, dtype=int)
     count = 0
@@ -62,6 +66,7 @@ def make_empty_places_list(seasons, places):
         seasons_out.append(season_tmp)
     return seasons_out
 
+
 def make_places_list(seasons, percentage_to_explore, block_size, whole_place_at_once,
                      single_place_per_batch,
                      dataset_weight, places):
@@ -71,10 +76,8 @@ def make_places_list(seasons, percentage_to_explore, block_size, whole_place_at_
     # make numpy array of booleans for each place for each season
     percentage_to_explore = percentage_to_explore * dataset_weight
 
-
-
     if percentage_to_explore == 0.0:
-        #return empty array
+        # return empty array
         return make_empty_places_list(seasons, places), 0
     else:
         picked_total_count = seasons * percentage_to_explore * places
@@ -91,7 +94,7 @@ def make_places_list(seasons, percentage_to_explore, block_size, whole_place_at_
             hole = random.randint(0, int(hole_size * 2))
             current_block = current_pose + hole
             if current_block + block_size >= seasons * places:
-                #print("generated only " + str(i * block_size) + " out of " + str(picked_total_count) + " :%" + str(
+                # print("generated only " + str(i * block_size) + " out of " + str(picked_total_count) + " :%" + str(
                 #    i * block_size / picked_total_count))
                 break
             batch_starts.append(current_block)
@@ -103,7 +106,7 @@ def make_places_list(seasons, percentage_to_explore, block_size, whole_place_at_
         current_batch_size = 0
         for place in range(places * seasons):
 
-            if current_batch_size == block_size :
+            if current_batch_size == block_size:
                 current_batch_start = current_batch_start + 1
                 current_batch_size = 0
 
@@ -119,23 +122,22 @@ def make_places_list(seasons, percentage_to_explore, block_size, whole_place_at_
             places_tmp.append(1)
 
     seasons_out = []
-    #split places_tmp into seasons count of arrays
+    # split places_tmp into seasons count of arrays
     for season in range(seasons):
         season_tmp = []
         for place in range(places):
             season_tmp.append(places_tmp[season * places + place])
         seasons_out.append(season_tmp)
 
-
     return seasons_out, count
 
 
 def save_strategy(name, experiments_path, percentage_to_explore, block_size, whole_place_at_once,
-                  single_place_per_batch, dataset_weights, places_out):
+                  single_place_per_batch, dataset_weights, places_out, places_weights):
     try:
         os.mkdir(os.path.join(experiments_path, name))
     except FileExistsError:
-        #print("Strategy already exists " + os.path.join(experiments_path, name))
+        # print("Strategy already exists " + os.path.join(experiments_path, name))
         pass
     try:
         os.mkdir(os.path.join(experiments_path, name) + "/plots")
@@ -148,7 +150,8 @@ def save_strategy(name, experiments_path, percentage_to_explore, block_size, who
         "block_size": block_size,
         "whole_places": whole_place_at_once,
         "uniform_places": single_place_per_batch,
-        "dataset_weights": dataset_weights
+        "dataset_weights": dataset_weights,
+        "places_weights": places_weights
     }
     json_object = json.dumps(dictionary, cls=NumpyArrayEncoder)
     with open(os.path.join(experiments_path, name) + "/input.json", "w") as outfile:
@@ -157,54 +160,71 @@ def save_strategy(name, experiments_path, percentage_to_explore, block_size, who
 
 def strategy_creator(name, experiments_path, percentage_to_explore, block_size=5,
                      whole_place_at_once=False, single_place_per_batch=False, dataset_weight=np.ones(2)):
-    #picked_diff = (1007.0*1007.0-1007.0)/(30.0*30.0-30.0) * 7.0/271.0
-
+    # picked_diff = (1007.0*1007.0-1007.0)/(30.0*30.0-30.0) * 7.0/271.0
 
     places_out_cestlice, c1 = make_places_list(30, percentage_to_explore, block_size, whole_place_at_once,
-                                           single_place_per_batch,
-                                           dataset_weight[0], places=271)
+                                               single_place_per_batch,
+                                               dataset_weight[0], places=271)
     places_out_strands, c2 = make_places_list(1007, percentage_to_explore, block_size, whole_place_at_once,
-                                          single_place_per_batch,
-                                          dataset_weight[1], places=7)
-    #print(percentage_to_explore,c1,c2,c1+c2)
+                                              single_place_per_batch,
+                                              dataset_weight[1], places=7)
+    # print(percentage_to_explore,c1,c2,c1+c2)
     save_strategy(name, experiments_path, percentage_to_explore, block_size, whole_place_at_once,
                   single_place_per_batch, dataset_weight, [places_out_cestlice, places_out_strands])
     return [places_out_cestlice, places_out_strands]
+
 
 def biased_strategy_creator(name, experiments_path, percentage_to_explore, block_size=5,
-                        whole_place_at_once=False, single_place_per_batch=False, dataset_weight=np.ones(2),
+                            whole_place_at_once=False, single_place_per_batch=False, dataset_weight=np.ones(2),
                             places_weights=None):
-
-
     places_out_cestlice, c1 = make_places_list_by_places(30, percentage_to_explore, block_size, whole_place_at_once,
-                                           single_place_per_batch,
-                                           dataset_weight[0], places=271,places_weights=places_weights)
+                                                         single_place_per_batch,
+                                                         dataset_weight[0], places=271, places_weights=places_weights)
     places_out_strands, c2 = make_places_list_by_places(1007, percentage_to_explore, block_size, whole_place_at_once,
-                                          single_place_per_batch,
-                                          dataset_weight[1], places=7,places_weights=places_weights)
-    #print(percentage_to_explore,c1,c2,c1+c2)
+                                                        single_place_per_batch,
+                                                        dataset_weight[1], places=7, places_weights=places_weights)
+    # print(percentage_to_explore,c1,c2,c1+c2)
     save_strategy(name, experiments_path, percentage_to_explore, block_size, whole_place_at_once,
-                  single_place_per_batch, dataset_weight, [places_out_cestlice, places_out_strands])
+                  single_place_per_batch, dataset_weight, [places_out_cestlice, places_out_strands], places_weights=places_weights)
     return [places_out_cestlice, places_out_strands]
-def make_multiple_strategies():
+
+
+def make_multiple_strategies(biased=False):
     a = np.arange(0.02, 0.20, 0.02)
     b = np.arange(0.20, 0.50, 0.05)
     c = np.arange(0.50, 0.99, 0.2)
     percentage_to_explore_list = np.concatenate([a, b, c])
-    percentage_to_explore_list = np.array([0.02, 0.05, 0.10, 0.15, 0.20, 0.30, 0.40, 0.50, 0.60, 0.80, 0.99])
+    percentage_to_explore_list = np.array([0.01, 0.02, 0.03, 0.04, 0.05, 0.07, 0.10, 0.12, 0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60])
     block_size_list = [1]
     whole_place_at_once_list = [True]
     single_place_per_batch_list = [True]
-    place_weight_randomness_list = [np.array([0.0, 1.0])]
+    dataset_weights = [np.array([0.0, 1.0])]
+    place_weights = [np.array([0.0, 1.0])]
+    if biased:
+        # for strands places: krajnas, office street, office stairs, kitchen, office outside entrance, sofas outside, office outside, office outisde 2
+        place_weights_contents = [np.array([1.0, 1.0, 1.0, 1.0, 0.2, 0.2, 0.2, 0.2]),  # outside less
+                         np.array([0.2, 0.2, 0.2, 0.2, 1.0, 1.0, 1.0, 1.0]),  # inside less
+                         np.array([1.0, 1.0, 1.0, 0.2, 1.0, 1.0, 1.0, 1.0]),  # kitchen less
+                         ]
+        place_weights = [0, 1, 2]
     a = [percentage_to_explore_list, block_size_list, whole_place_at_once_list, single_place_per_batch_list,
-         place_weight_randomness_list]
+         dataset_weights, place_weights]
     strategies = list(itertools.product(*a))
     names = []
-    for strategy in strategies:
+    s = []
+    for idx, strategy in enumerate(strategies):
+        tmp_strategy = list(strategy)
         # name = f'{strategy[0]:.2f}_{strategy[1]:d}_{strategy[2]:d}_{strategy[3]:d}'
-        name = f'{strategy[0]:.2f}_{strategy[1]:d}_{strategy[2]:d}_{strategy[3]:d}_{strategy[4][0]:.1f}{strategy[4][1]:.1f}'
+        if not biased:
+            name = f'{strategy[0]:.2f}_{strategy[1]:d}_{strategy[2]:d}_{strategy[3]:d}_{strategy[4][0]:.1f}{strategy[4][1]:.1f}'
+            s.append(tmp_strategy)
+        else:
+            name = f'{strategy[0]:.2f}_{strategy[1]:d}_{strategy[2]:d}_{strategy[3]:d}_{strategy[4][0]:.1f}{strategy[4][1]:.1f}_{strategy[5]:d}'
+            tmp_strategy[5] = place_weights_contents[strategy[5]]
+            s.append(tmp_strategy)
         names.append(name)
-    return names, strategies
+
+    return names, s
 
 
 def make_test_strategy():
@@ -217,7 +237,8 @@ def make_test_strategy():
           place_weight_randomness_list]]
     return ["none"], a
 
-def make_dummy_strategys(count = 10):
+
+def make_dummy_strategys(count=10):
     out = []
     names = []
     for i in range(count):
@@ -227,19 +248,21 @@ def make_dummy_strategys(count = 10):
         single_place_per_batch = False
         place_weight_randomness_list = np.array(np.ones(2))
         a = [[percentage_to_explore, block_size, whole_place_at_once, single_place_per_batch,
-          place_weight_randomness_list]]
+              place_weight_randomness_list]]
         out.append(a[0])
         names.append("0.00_0_0_0_0.00." + str(i))
     return names, out
 
+
 if __name__ == "__main__":
-    names, strategies = make_multiple_strategies()
-    #names, strategies = make_test_strategy()
-    #names, strategies = make_dummy_strategys(10)
-    names, strategies = make_multiple_strategies_biased()
+    names, strategies = make_multiple_strategies(biased=True)
+    # names, strategies = make_test_strategy()
+    # names, strategies = make_dummy_strategys(10)
     for index, strategy in enumerate(strategies):
-        #strategy_creator(names[index], experiments_path, strategy[0], block_size=strategy[1],whole_place_at_once=strategy[2], single_place_per_batch=strategy[3],dataset_weight=strategy[4])
-        biased_strategy_creator(names[index], experiments_path, strategy[0], block_size=strategy[1], whole_place_at_once=strategy[2], single_place_per_batch=strategy[3],dataset_weight=strategy[4], places_weights=strategy[5])
+        # strategy_creator(names[index], experiments_path, strategy[0], block_size=strategy[1],whole_place_at_once=strategy[2], single_place_per_batch=strategy[3],dataset_weight=strategy[4])
+        biased_strategy_creator(names[index], experiments_path, strategy[0], block_size=strategy[1],
+                                whole_place_at_once=strategy[2], single_place_per_batch=strategy[3],
+                                dataset_weight=strategy[4], places_weights=strategy[5])
     names.sort()
-    #names.append("none")
+    # names.append("none")
     process(names)
