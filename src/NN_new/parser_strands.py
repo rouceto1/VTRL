a966fe5d088a7b9b47042b3704533d0ca11dbd3c
rouceto1-t14 <rouceto1@fel.cvut.cz>
2023-12-11 16:25:10 +0100

    chaching benchmark 2

--- src/NN_new/parser_strands.py
+++ src/NN_new/parser_strands.py
@@ -36,11 +36,11 @@ class StrandsImgPairDataset(Dataset):
                     path2 = pair[1]
                     if path1 not in self.image_paths:
                         self.image_paths.append(path1)
-                        self.images.append(get_img(path1).to(device))
+                        self.images.append(get_img(path1))
                         idx += 1
                     if path2 not in self.image_paths:
                         self.image_paths.append(path2)
-                        self.images.append(get_img(path2).to(device))
+                        self.images.append(get_img(path2))
                         idx += 1
                     self.data.append((self.image_paths.index(path1), self.image_paths.index(path2), 0, i))
 
@@ -138,7 +138,7 @@ class Strands(StrandsImgPairDataset):
         else:
             heatmap = get_smooth_heatmap(self.smoothness, self.width, crop_start, self.fraction, self.crop_width)
         # plot_heatmap(source, target, cropped_target, displ, heatmap)
-        return source.to(self.device), cropped_target.to(self.device), displ, heatmap.to(self.device)
+        return source, cropped_target, displ, heatmap.to(self.device)
 
     def __getitem__(self, idx):
         if self.train:
