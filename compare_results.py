e6d0c53be71950f5bdb698cac98dfb48c5f13139
rouceto1-t14 <rouceto1@fel.cvut.cz>
2024-03-18 21:08:05 +0100

    Fixed lost plotting file, split results to be more readable

--- compare_results.py
+++ compare_results.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from python.teach.mission import Mission, Strategy
 from python.grading.std_plots import *
+from python.grading.results import Results
 import matplotlib.pyplot as plt
 import matplotlib.cm as cum
 import numpy as np
@@ -10,269 +10,6 @@ import pickle
 import os
 
 pwd = os.getcwd()
-
-strategy_keys = ["uptime", "block_size", "dataset_weights", "used_teach_count", "preferences", "time_limit",
-                 "time_advance", "change_rate", "iteration", "duty_cycle", "preteach", "roll_data", "ee_ratio"]
-df_keys_old = ["uptime", "new_param", "block_size", "dataset_weights", "used_teach_count", "preferences", "time_limit",
-           "time_advance", "change_rate", "iteration", "duty_cycle", "preteach", "roll_data", "train_time",
-           "metrics_type", "train_time", "ee_ratio"]
-df_keys = ["uptime", "new_param", "block_size", "dataset_weights", "used_teach_count", "preferences", "time_limit",
-           "time_advance", "change_rate", "iteration", "duty_cycle", "preteach", "roll_data", "train_time"
-    , "method_type", "train_time", "ee_ratio"]
-df_grading_keys = ["AC_fm_integral", "AC_nn_integral", "streak_integral", "AC_fm", "AC_nn"]
-
-
-def get_only_keys(keys, dictionary):
-    out = {}
-    for key in dictionary:
-        if key in keys:
-            out[key] = dictionary[key]
-    return out
-
-
-class Results:
-    def __init__(self, path):
-        print("-------------------------------------------------")
-        self.path = path
-        self.missions, self.generator = self.load_missions(path)
-        self.output_graph_path = os.path.join(path, "compare.png")
-        print(path)
-        if self.generator is not None:
-            for g in self.generator:
-                print(g.replace("\n", ""))
-        # for m in self.missions:
-        # m.c_strategy.print_parameters()
-        # self.t = self.get_N_HexCol(len(self.data))
-
-    def add_missions(self, path):
-        print("-------------------------------------------------")
-        print(path)
-        new, gen2 = self.load_missions(path)
-        self.missions.extend(new)
-        if gen2 is not None:
-            for g in gen2:
-                print(g.replace("\n", ""))
-        # for m in new:
-        # m.c_strategy.print_parameters()
-
-    def load_missions(self, path):
-        experiments_path = os.path.join(pwd, path)
-        gen = None
-        if os.path.exists(os.path.join(experiments_path, "generator.txt")):
-            with open(os.path.join(experiments_path, "generator.txt")) as f:
-                gen = f.readlines()
-        paths = [item for item in os.listdir(experiments_path) if os.path.isdir(os.path.join(experiments_path, item))]
-        paths.sort()
-        missions = []
-        for mission_name in paths:
-            mission = Mission(int(mission_name))
-            mission = mission.load(os.path.join(path, mission_name))
-            missions.append(mission)
-        return missions, gen
-
-    def is_strategy_same_as_params(self, strategy, params, exclude):
-
-        p = get_only_keys(strategy_keys, vars(params))
-        m = get_only_keys(strategy_keys, vars(strategy))
-        e = get_only_keys(strategy_keys, vars(exclude))
-        # compare dicts p and m and return true if non None values from p are same in m
-        for key in p:
-            if p[key] is not None:
-                if key in ["used_teach_count"]:
-                    if p[key] == 0:
-                        continue
-                    if p[key] < m[key]:
-                        return False
-                elif key in ["preferences","dataset_weights"]:
-                    if not np.array_equal(p[key], m[key]):
-                        return False
-                elif p[key] != m[key]:
-                    return False
-
-        for key in e:
-            if e[key] is not None:
-                if key in ["used_teach_count"]:
-                    if e[key] == 0:
-                        continue
-                    if e[key] >= m[key]:
-                        return False
-                elif key in ["preferences"]:
-                    if np.array_equal(p[key], m[key]):
-                        return False
-                elif e[key] == m[key]:
-                    return False
-
-        return True
-
-    def get_name_from_strategy_params(self, strategy):
-        # return names and values of stategy variables taht are not none
-
-        p = get_only_keys(strategy_keys, vars(strategy))
-        name = ""
-        for key in p:
-            if p[key] is not None:
-                if key in ["used_teach_count"]:
-                    if p[key] == 0:
-                        continue
-                name += key + "=" + str(p[key])
-        return name
-
-    def concatenate_params(self, strategy, params):
-        out = []
-        for p in params:
-            v = getattr(strategy, p)
-            out.append(v)
-        strategy.new_param = out
-
-    def filter_strategies(self, mission_params=None, stategy_params=None, exclude_strategy=None, sorting_params=None):
-
-        # dam inclusion param pro mise // napr mise se specifickym polem na startu
-        # dam inclusion param pro strategie //napr pouze 3. iterace
-        # dam sorting param
-        # ono to vyradi a sortne v poradi strategie
-        values = []
-        strategies = []
-        names = []
-        override = False
-        idx = 0
-        for mission in self.missions:
-            if len(mission.old_strategies) == 0:
-                continue
-            if mission.old_strategies[0] == mission_params:
-                override = True
-            for strategy in mission.old_strategies:
-                if not self.is_strategy_same_as_params(strategy, stategy_params, exclude_strategy) or override:
-                    continue
-
-                strategy.name = idx
-                idx += 1
-                if "ee_ratio" in sorting_params:
-                    if not hasattr(strategy, "ee_ratio"):
-                        if strategy.change_rate == -1:
-                            strategy.ee_ratio = 0.0
-                        elif strategy.change_rate == 1:
-                            strategy.ee_ratio = 1.0
-                        else:
-                            continue
-                    else:
-                        if strategy.change_rate == -1:
-                            strategy.ee_ratio = 0.0
-                if sorting_params == "preferences":
-                    if len(values) == 0:
-                        values.append(strategy.preferences)
-                    is_in_list = np.any(np.all(strategy.preferences == values, axis=1))
-                    if not is_in_list:
-                        values.append(strategy.preferences)
-                elif len(sorting_params) > 1:
-                    self.concatenate_params(strategy, sorting_params)
-                    if getattr(strategy, "new_param") not in values:
-                        values.append(getattr(strategy, "new_param"))
-                else:
-                    if getattr(strategy, sorting_params[0]) not in values:
-                        values.append(getattr(strategy, sorting_params[0]))
-
-                strategies.append(strategy)
-
-        # TODO make this by sortin_params, prolly use getattribute....
-        if sorting_params != "preferences" and len(sorting_params) == 1:
-            strategies.sort(key=lambda x: getattr(x, sorting_params[0]), reverse=False)
-        colors = get_N_HexCol(len(strategies))
-        return strategies, colors, values, self.make_pandas_df(strategies, sorting_paramteres=sorting_params)
-
-    def get_corr_from_strategy(self, startegies, var=[], type="standart"):
-        out = []
-        text = []
-        for v in var:
-            o = []
-            t = []
-            for s in startegies:
-                if type == "grade":
-                    for grade in s.grading:
-                        if grade is None:
-                            continue
-                        if grade.name == "grief":
-                            continue
-                        o.append(getattr(grade, v))
-                        t.append(str(v))
-                else:
-                    o.append(getattr(s, v))
-                    t.append(str(v))
-            out.append(o)
-            text.append(t)
-        return out, text
-
-    def correlate(self, filter_strategy=Strategy(), sorting_parameters=["change_rate"], correlation_var=[],
-                  grading_var=["AC_fm_integral"], exclude_strategy=Strategy()):
-        strategies_to_corelate, colors, values, df = self.filter_strategies(stategy_params=filter_strategy,
-                                                                            sorting_params=sorting_parameters,
-                                                                            exclude_strategy=exclude_strategy)
-
-        v1, t1 = (self.get_corr_from_strategy(strategies_to_corelate, correlation_var))
-        v2, t2 = (self.get_corr_from_strategy(strategies_to_corelate, grading_var, type="grade"))
-        corr = np.concatenate([v1, v2])
-        names = np.concatenate([correlation_var, grading_var])
-
-        R2 = np.corrcoef(corr)
-        df_cm = pd.DataFrame(R2, index=[i for i in names],
-                             columns=[i for i in names])
-        plt.figure()
-        sn.heatmap(df_cm, annot=True)
-
-    def make_pandas_df(self, strategies, sorting_paramteres=None):
-        out = []
-
-        for s in strategies:
-            a = get_only_keys(df_keys, vars(s))
-            b = get_only_keys(df_grading_keys, vars(s.grading[0])) ## TODO Change this based on cestlice or other, This now only loads gradoing 0 = cestlice only (for campatilityty with old data 0is default)
-            #Probably make it so it has "AC_fm_integral, AC_fm_integral_cestlice, AC_fm_integral_strands" and so on
-            out.append(a | b)
-        df = pd.DataFrame(out)
-        df['preteach'] = df.apply(self.agreagate_preteach, axis=1)
-        df['roll_data'] = df.apply(self.agreagate_roll_data, axis=1)
-        df['change_rate'] = df.apply(self.name_change_rate, axis=1)
-        df["metrics_type"] = df.apply(self.name_metrics, axis=1)
-        df["method_type"] = df["metrics_type"]
-        df['roll_pretech'] = df['roll_data'] + " " + df['preteach']
-        df['real_uptime'] = df['uptime'] * df['duty_cycle']
-        return df
-
-    def agreagate_preteach(self, dataframe):
-        if dataframe["preteach"] == True:
-            return "Continued weights"
-        else:
-            return "Initial weights"
-
-    def agreagate_roll_data(self, dataframe):
-        if dataframe["roll_data"] == True:
-            return "New data /\n"
-        else:
-            return "All data /\n"
-
-    def name_change_rate(self, dataframe):
-        if dataframe["change_rate"] == 1.0:
-            return "Adaptive exploration"
-        elif dataframe["change_rate"] == 0.0:
-            return "Static exploration"
-        elif dataframe["change_rate"] == -1.0:
-            return "Random exploration"
-
-    def name_metrics(self, dataframe):
-        try:
-            if dataframe["metrics_type"] == 0:
-                return "Enthropy threshold"
-            elif dataframe["metrics_type"] == 1:
-                return "Enthropy"
-            elif dataframe["metrics_type"] == 2:
-                return "Ratio"
-        except:
-            if dataframe["method_type"] == 0:
-                return "Enthropy threshold"
-            elif dataframe["method_type"] == 1:
-                return "Enthropy"
-            elif dataframe["method_type"] == 2:
-                return "Ratio"
-
-
 def comparison_to_old_system():
     results = Results(os.path.join("backups", "compare"))
     #results.add_missions(os.path.join("backups", "ee"))
@@ -315,12 +52,11 @@ def get_preferences(results):
         for strat in mission.old_strategies:
             if strat.change_rate == 1:
                 if strat.duty_cycle == 5.0:
-                    m.append(strat.preferences)
+                    m.append(strat.place_weights)
         if strat.change_rate == 1:
             if strat.duty_cycle == 5.0:
                 preferences.append(m)
     plot_preferences(preferences)
-
 
 
 def get_dc_vs_change_rate():
@@ -364,17 +100,43 @@ def get_timigs():
 
 def get_progress():
     #convergence of preferences graph
-    results = results = Results(os.path.join("backups", "compare"))
+    results = Results(os.path.join("backups", "compare"))
 
     get_preferences(results)
+
+def print_df_to_csv(dfs, path, sorting_parameter, grouping = None):
+    #saves infromatio for eahc graphs DF to csv
+    #First obtain pairs (touples, things to compare)
+    out = []
+    separe_datasets = False
+    if separe_datasets:
+        for d in dfs:
+            pass
+            #get lines that have same variables
+    else:
+        pass
+    #than seve the csv
+
+    with open():
+        pass
+
 
 def get_graphs_for_paper():
     results = Results(os.path.join("backups", "c_basic"))
     results.add_missions(os.path.join("backups", "c_up_dc_075_025"))
+    ## those two: [uptime 025 05 075], [chnge_rate 1 0 -1], [duy_cycle 025 05 075]
+
+
     #DC vs changer rate
     #results = Results(os.path.join("backups", "uptime"))
-    scatter_violin(results, filter_strategy=Strategy(iteration=6), exclude_strategy=Strategy(duty_cycle=3.0),variable="AC_fm_integral",
+    dfs = scatter_violin(results, filter_strategy=Strategy(iteration=6), exclude_strategy=Strategy(duty_cycle=3.0),variable="AC_fm_integral",
                    sorting_paramteres=["duty_cycle", "change_rate"],
+                    plot_params = ["Relative improvement for each strategy over different duty cycles", "Duty cycle", "AC Integral",[0.435,0.475],'lower right']
+    )
+
+
+    scatter_violin(results, filter_strategy=Strategy(iteration=6), exclude_strategy=Strategy(duty_cycle=3.0),variable="AC_fm_integral",
+                   sorting_paramteres=["change_rate", "duty_cycle","uptime"], grouping="real_uptime",
                     plot_params = ["Relative improvement for each strategy over different duty cycles", "Duty cycle", "AC Integral",[0.435,0.475],'lower right']
     )
 
@@ -382,6 +144,8 @@ def get_graphs_for_paper():
     #results = Results(os.path.join("backups", "metrics_2"))
     #results.add_missions(os.path.join("backups", "metrics"))
     #TODO results.add_missions(os.path.join("backups", "metrics_3"))
+    results = Results(os.path.join("backups", "c_methods"))
+    ## this one [uptime 025 033], [chnge_rate 1], [duy_cycle 025 033], [method_type 0 1 2]
     scatter_violin(results, filter_strategy=Strategy(iteration=6,roll_data=False, change_rate=1), variable="AC_fm_integral",\
                    ## TODO OLD sorting_paramteres=["metrics_type"],
                    sorting_paramteres=["method_type"],
--- python/grading/std_plots.py
+++ python/grading/std_plots.py
@@ -3,7 +3,7 @@ import colorsys
 import os
 import seaborn as sn
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 def get_N_HexCol(N=5):
     HSV_tuples = [(x * 1.0 / (N * 1.5), 1, 1) for x in range(N)]
@@ -37,23 +37,35 @@ def pure_plot(ax, df, sorting_paramteres, variable, grouping, plot_params):
 
 def scatter_violin(results, filter_strategy=Strategy(), variable="AC_fm_integral", exclude_strategy=Strategy(),
                    sorting_paramteres=["change_rate", "preteach", "roll_data"], grouping="roll_pretech",
-                   plot_params=[]):
+                   plot_params=[],all_four=False):
     stategy_c = filter_strategy
-    stategy_s = filter_strategy
     stategy_c.dataset_weights = [1.0, 0.0]
-    strategies_to_plot, colors, values, dfc = results.filter_strategies(stategy_params=stategy_c,
+    strategies_to_plot, colors, values, dfc0 = results.filter_strategies(stategy_params=stategy_c,
                                                                        sorting_params=sorting_paramteres,
-                                                                       exclude_strategy=exclude_strategy)
+                                                                       exclude_strategy=exclude_strategy, ground_truth_index=0)
+    strategies_to_plot, colors, values, dfc1 = results.filter_strategies(stategy_params=stategy_c,
+                                                                       sorting_params=sorting_paramteres,
+                                                                       exclude_strategy=exclude_strategy, ground_truth_index=1)
     stategy_s = filter_strategy
     stategy_s.dataset_weights = [0.0, 1.0]
-    strategies_to_plot, colors, values, dfs = results.filter_strategies(stategy_params=stategy_s,
+    strategies_to_plot, colors, values, dfs0 = results.filter_strategies(stategy_params=stategy_s,
                                                                        sorting_params=sorting_paramteres,
-                                                                       exclude_strategy=exclude_strategy)
-    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
-    pure_plot(ax1, dfc, sorting_paramteres, variable, grouping, [plot_params[0]+ "\nCestlice ", "", plot_params[2], [], "upper left"])
-    pure_plot(ax2, dfs, sorting_paramteres, variable, grouping, ["Strands", plot_params[1],plot_params[2], [], ""])
+                                                                       exclude_strategy=exclude_strategy, ground_truth_index=0)
+    strategies_to_plot, colors, values, dfs1 = results.filter_strategies(stategy_params=stategy_s,
+                                                                       sorting_params=sorting_paramteres,
+                                                                       exclude_strategy=exclude_strategy, ground_truth_index=1)
+    if all_four:
+        fig, (ax1, ax2) = plt.subplots(2, 2, sharex=True)
+        pure_plot(ax1[0], dfc0, sorting_paramteres, variable, grouping, [plot_params[0]+ "\nCestlice cestlice ", "", plot_params[2], [], "upper left"])
+        pure_plot(ax2[0], dfs0, sorting_paramteres, variable, grouping, ["Strands cestlice", plot_params[1],plot_params[2], [], ""])
 
-
+        pure_plot(ax1[1], dfc1, sorting_paramteres, variable, grouping, ["Cestlice strands", "", plot_params[2], [], ""])
+        pure_plot(ax2[1], dfs1, sorting_paramteres, variable, grouping, ["Strands strands", plot_params[1],plot_params[2], [], ""])
+    else:
+        fig, (ax1, ax2) = plt.subplots(1, 2, sharex=True)
+        pure_plot(ax1, dfc0, sorting_paramteres, variable, grouping, [plot_params[0]+ "\nCestlice cestlice ", "", plot_params[2], [], "upper left"])
+        pure_plot(ax2, dfs1, sorting_paramteres, variable, grouping, ["Strands strands", plot_params[1],plot_params[2], [], ""])
+    return [dfc0,dfs1]
 
 def plot_std(results, filter_strategy=Strategy(), exclude_strategy=Strategy(),
              sorting_paramteres=[]):
@@ -65,8 +77,34 @@ def plot_std(results, filter_strategy=Strategy(), exclude_strategy=Strategy(),
         exclude_strategy=exclude_strategy)
 
 
-def plot_preferences(preferences):
-    pass
+def plot_preferences(pref):
+    #plot preferences in time manner
+    # preferences = [ line1 [ [val1, val2, val3, ... ], [val1, val2, val3, ... ]], line2 [ [val1, val2, val3, ... ], [val1, val2, val3, ... ]], ...]
+    #plot error band computed for each line
+    #plot mean of each line
+
+    fig , ax = plt.subplots(2)
+    mean = []
+    std = []
+    if len(pref) != 2:
+        pref = [pref]
+    for preferences in pref:
+        for index in range(len(preferences[1])):
+            m = []
+            for p in preferences:
+                m.append(p[index])
+            mean.append(np.mean(m ,axis=0))
+            std.append(np.std(m,axis=0))
+        iter = range(len(mean))
+        plt.legend(mean, iter)
+        mean = np.array(mean)
+        std = np.array(std)
+        ax[0].plot(iter, mean,"x",linestyle="-")
+        for i in range(len(mean)):
+            ax[0].fill_between(iter, mean.T[i] - std.T[i], mean.T[i] + std.T[i], alpha=0.2)
+        plt.show()
+
+
 
 
 def stack_violin_iterations(results, filter_strategy=Strategy(iteration=6, roll_data=True), exclude_strategy=Strategy(),
